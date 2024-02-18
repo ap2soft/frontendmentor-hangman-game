82ccda423f6d0c883a80cf45938d2c6a88bb3dc3
Andrew Ponomarev <spargo32@gmail.com>
2024-02-18 19:16:47 +0300

    Set up the font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 class="text-center text-4xl font-bold text-blue-500">Hello world</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam
+    aspernatur consequuntur cupiditate debitis dolorum eligendi exercitationem,
+    explicabo fugit id ipsa laudantium minus neque nesciunt, nulla perferendis
+    tenetur ullam vitae?
+  </p>
 </template>
 
 <script setup></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import("tailwindcss").Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,vue}"
-  ], theme: {
-    extend: {}
+  content: ['./index.html', './src/**/*.{js,vue}'],
+  theme: {
+    fontSize: {
+      base: '1.625rem',
+      lg: '2rem',
+      xl: '3rem',
+      '2xl': '5.5rem',
+      '3xl': '8.5rem',
+    },
+    extend: {
+      fontFamily: {
+        sans: ["'Mouse Memoirs'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: []
-};
-
+  plugins: [],
+}
